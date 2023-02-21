a89e941eb4bfa976660f170f1fc75f9cc163d17f
Satyam Arora <64372833+Satyamarora373@users.noreply.github.com>
2023-02-21 14:46:00 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,34 +3,16 @@ import Hero from "../components/hero";
 import Navbar from "../components/navbar";
 import SectionTitle from "../components/sectionTitle";
 
-import { benefitOne, benefitTwo } from "../components/data";
-import Video from "../components/video";
-import Benefits from "../components/benefits";
 import Footer from "../components/footer";
 import Testimonials from "../components/testimonials";
 import Cta from "../components/cta";
-import Faq from "../components/faq";
-import PopupWidget from "../components/popupWidget";
-import Container from "../components/container";
-import Carousel from "../components/carousel";
 import Tabs from "../components/tab"
-//import dynamic from "next/dynamic";
 
-// const Video = dynamic(() => import("../components/video"));
-
-// const Benefits = dynamic(() => import("../components/benefits"));
-// const Footer = dynamic(() => import("../components/footer"));
-// const Testimonials = dynamic(() => import("../components/testimonials"));
-// const Cta = dynamic(() => import("../components/cta"));
-// const Faq = dynamic(() => import("../components/faq"));
-
-// const PopupWidget = dynamic(() => import("../components/popupWidget"));
 
 export default function Home() {
   return (
     <>
       <Head>
-        {/* <title>Nextly - Free Nextjs & TailwindCSS Landing Page Template</title> */}
         <meta
           name="description"
           content="Nextly is a free landing page template built with next.js & Tailwind CSS"
@@ -137,13 +119,7 @@ export default function Home() {
       </section>
       <Tabs />
       
-      {/* <SectionTitle pretitle="FAQ" title="Frequently Asked Questions">
-        Answer your customers possible questions here, it will increase the
-        conversion rate as well as support or chat requests.
-      </SectionTitle>
-      <Faq /> */}
       <Cta />
-      {/* <Carousel/> */}
       <Footer />
       
     </>
